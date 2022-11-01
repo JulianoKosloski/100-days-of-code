5d11192e00fcce5bb5ae46c09c3c6fa2dc7b18a9
JulianoKosloski <juliano.koslos@gmail.com>
2022-11-01 13:43:38 -0300

    testing open weahter api

---
+++ auth.py
@@ -0,0 +1,21 @@
+import requests as re
+
+# many APIs have paid tiers of API keys so they can sell access to the data they keep
+
+# https://api.openweathermap.org/data/2.5/forecast?lat=-25.480877&lon=-49.304424&appid=94ed3e55937bfc2c54db3a3045fb0f86
+weatherEndpoint = "https://api.openweathermap.org/data/2.5/forecast"
+myKey = "94ed3e55937bfc2c54db3a3045fb0f86"
+lat = -25.480877
+lon = -49.304424
+
+params = {
+    "lat": lat,
+    "lon": lon,
+    "appid": myKey 
+}
+
+response = re.get(weatherEndpoint, params)
+print(response.json())
+
+
+
